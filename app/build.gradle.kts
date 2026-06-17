52c9c24706897d4c666c2942646d9ef597bae081
WeroMessenger <werovisualics@gmail.com>
2026-06-17 20:51:12 +0500

    chore: add task to copy debug apk to root directory

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,3 +119,8 @@ dependencies {
   "ksp"(libs.androidx.room.compiler)
   "ksp"(libs.moshi.kotlin.codegen)
 }
+
+tasks.register<Copy>("copyApk") {
+  from(layout.buildDirectory.file("outputs/apk/debug/app-debug.apk"))
+  into(rootDir)
+}
